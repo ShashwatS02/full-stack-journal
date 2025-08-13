0a99677532f6e3235aca69c888a39ac8a78df98d
ShashwatS02 <shashawat15@gmail.com>
2025-08-14 01:49:20 +0530

    feat: added part7, part8, and part9_adv to Learn-JS-Foundation

---
+++ Learn-JS-Foundation/part9_adv/thisContext.js
@@ -0,0 +1,16 @@
+const person = {
+  name: "Shashwat",
+  greet() {
+    console.log(`Hi, I am ${this.name}`);
+  },
+};
+
+person.greet();
+
+const greetFunction = person.greet;
+greetFunction();
+
+const boundGreet = person.greet.bind({ name: "John" });
+boundGreet();
+
+//do some research on this bind, call and apply
